881bca34a843a5ebc57d5f9b51cd56751eb721b6
william18 <adu.selasie@gmail.com>
2021-01-14 00:17:50 +0000

    first commit

---
+++ src/main/java/com/meek/freshapp/FreshappApplication.java
@@ -0,0 +1,13 @@
+package com.meek.freshapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meek/freshapp/FreshappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meek.freshapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
